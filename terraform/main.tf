9da1d4150db4489d62d54d77c7c1c8e04b506e8b
Andrew Newhouse <andrewwn@gmail.com>
2026-03-25 15:29:26 -0400

    fixing tf format

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,35 +10,35 @@ locals {
 # --- 1. SIMPLE & BROWSER MONITORS ---
 resource "newrelic_synthetics_monitor" "monitor" {
   # Only pick monitors where type is SIMPLE or BROWSER
-  for_each = { 
-    for m in local.all_monitors : m.name => m 
-    if m.type == "SIMPLE" || m.type == "BROWSER" 
+  for_each = {
+    for m in local.all_monitors : m.name => m
+    if m.type == "SIMPLE" || m.type == "BROWSER"
   }
 
-  name      = each.value.name
-  type      = each.value.type
-  uri       = lookup(each.value, "uri", null)
-  period    = each.value.period
-  status    = "ENABLED"
+  name                 = each.value.name
+  type                 = each.value.type
+  uri                  = lookup(each.value, "uri", null)
+  period               = each.value.period
+  status               = "ENABLED"
   runtime_type_version = "100"
   runtime_type         = "CHROME_BROWSER"
-  locations_public = lookup(each.value, "locations", ["US_EAST_1"])
+  locations_public     = lookup(each.value, "locations", ["US_EAST_1"])
 }
 
 # --- 2. SSL (CERT) CHECK MONITORS ---
 resource "newrelic_synthetics_cert_check_monitor" "ssl_monitor" {
   # Only pick monitors where type is CERT_CHECK
-  for_each = { 
-    for m in local.all_monitors : m.name => m 
-    if m.type == "CERT_CHECK" 
+  for_each = {
+    for m in local.all_monitors : m.name => m
+    if m.type == "CERT_CHECK"
   }
 
-  name                  = each.value.name
-  domain                = each.value.domain
-  period                = each.value.period
-  status                = "ENABLED"
+  name                   = each.value.name
+  domain                 = each.value.domain
+  period                 = each.value.period
+  status                 = "ENABLED"
   certificate_expiration = 30 # Default to 30 days
-  locations_public      = lookup(each.value, "locations", ["US_EAST_1"])
+  locations_public       = lookup(each.value, "locations", ["US_EAST_1"])
 }
 
 # --- 3. UPDATED TAGGING LOGIC ---
@@ -48,8 +48,8 @@ resource "newrelic_entity_tags" "all_tags" {
 
   # This dynamic lookup finds the GUID regardless of which resource created it
   guid = (
-    contains(["SIMPLE", "BROWSER"], each.value.type) ? 
-    newrelic_synthetics_monitor.monitor[each.key].id : 
+    contains(["SIMPLE", "BROWSER"], each.value.type) ?
+    newrelic_synthetics_monitor.monitor[each.key].id :
     newrelic_synthetics_cert_check_monitor.ssl_monitor[each.key].id
   )
 
