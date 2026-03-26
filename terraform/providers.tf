a2507ebfbc4d7bd6722e51a665c122c35dd24fd6
Andrew Newhouse <andrewwn@gmail.com>
2026-03-26 16:30:15 -0400

    untangling providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,6 @@
-terraform {
-  required_providers {
-    newrelic = {
-      source = "newrelic/newrelic"
-    }
-  }
-}
-
 # Default provider (Required for initialization)
 provider "newrelic" {
+  alias      = "websystems"
   account_id = var.NEW_RELIC_ACCOUNT_ID
   api_key    = var.NEW_RELIC_API_KEY
   region     = var.NEW_RELIC_REGION
